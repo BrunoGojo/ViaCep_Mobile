2010bd428fbcb7c73a7e77c7cc10aeb928ca3397
BrunoGojo <brunardoxs84@gmail.com>
2024-11-28 03:04:51 -0300

    Final-Room

---
+++ app/src/main/java/com/example/viacep/domain/local/usecase/GetAddressByIdUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.viacep.domain.local.usecase
+
+import com.example.viacep.data.local.entity.AddressEntity
+import com.example.viacep.domain.local.repository.AddressLocalRepository
+import javax.inject.Inject
+
+class GetAddressByIdUseCase @Inject constructor(
+    private val repository: AddressLocalRepository
+){
+
+    suspend operator fun invoke(id: Long): AddressEntity? {
+        return repository.getAddressById(id)
+    }
+
+}
