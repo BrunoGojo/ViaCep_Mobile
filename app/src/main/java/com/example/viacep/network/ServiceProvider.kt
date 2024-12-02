91ae5e64909d67fecefe24702e56df02950f17eb
BrunoGojo <brunardoxs84@gmail.com>
2024-12-01 23:21:12 -0300

    Final-Room

--- app/src/main/java/com/example/viacep/network/ServiceProvider.kt
+++ app/src/main/java/com/example/viacep/network/ServiceProvider.kt
@@ -1,5 +1,9 @@
 package com.example.viacep.network
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -25,4 +29,16 @@ object ServiceProvider {
 
     fun <API> createService(apiClass: Class<API>): API = retrofit.create(apiClass)
 
+
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork
+            val capabilities = connectivityManager.getNetworkCapabilities(network)
+            return capabilities != null && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        } else {
+            val networkInfo = connectivityManager.activeNetworkInfo
+            return networkInfo != null && networkInfo.isConnected
+        }
+    }
 }
